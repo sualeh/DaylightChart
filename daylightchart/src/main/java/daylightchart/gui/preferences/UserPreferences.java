6e01ae102d164cbfbcc8e52c5e06c8cdd667db50
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2007-04-11 20:00:41 +0000

    Adding options
    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@37 68adf913-14b5-4d52-97c4-70b13c5eca14

---
+++ daylightchart/src/main/java/daylightchart/gui/preferences/UserPreferences.java
@@ -0,0 +1,154 @@
+package daylightchart.gui.preferences;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+/**
+ * User preferences for the GUI.
+ * 
+ * @author Sualeh Fatehi
+ */
+public class UserPreferences
+{
+
+  private enum PreferenceKeys
+  {
+    locations("daylightchart.locations"), chartOptions(
+      "daylightchart.chartOptions");
+
+    private final String key;
+
+    private PreferenceKeys(final String key)
+    {
+      this.key = key;
+    }
+
+    /**
+     * @return the key
+     */
+    public String getKey()
+    {
+      return key;
+    }
+
+  }
+
+  /**
+   * Main method. Clears all user preferences.
+   * 
+   * @param args
+   *        Command line arguments
+   */
+  public static void main(final String[] args)
+  {
+
+    String command = "list";
+    if (args.length > 0)
+    {
+      command = args[0];
+    }
+    if ("clear".equals(command))
+    {
+      new UserPreferences().clear();
+    }
+    else if ("list".equals(command))
+    {
+      new UserPreferences().listAllPreferences();
+    }
+    else
+    {
+      new UserPreferences().listAllPreferences();
+    }
+
+  }
+
+  private final Preferences preferences = Preferences.userNodeForPackage(this
+    .getClass());
+
+  public ChartOptions getChartOptions()
+  {
+    ChartOptions chartOptions = null;
+    final byte[] bytes = preferences.getByteArray(PreferenceKeys.chartOptions
+      .getKey(), new byte[0]);
+    try
+    {
+      // Deserialize from a byte array
+      final ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes));
+      chartOptions = (ChartOptions) in.readObject();
+      in.close();
+    }
+    catch (final Exception e)
+    {
+      chartOptions = new ChartOptions();
+    }
+    return chartOptions;
+  }
+
+  public String getLocations()
+  {
+    return preferences.get(PreferenceKeys.locations.getKey(), null);
+  }
+
+  public void setChartOptions(final ChartOptions chartOptions)
+  {
+    byte[] bytes;
+    try
+    {
+      // Serialize to a byte array
+      final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      final ObjectOutput out = new ObjectOutputStream(bos);
+      out.writeObject(chartOptions);
+      out.close();
+      // Get the bytes of the serialized object
+      bytes = bos.toByteArray();
+    }
+    catch (final Exception e)
+    {
+      bytes = new byte[0];
+    }
+    preferences.putByteArray(PreferenceKeys.chartOptions.getKey(), bytes);
+  }
+
+  public void setLocations(final String locations)
+  {
+    preferences.put(PreferenceKeys.locations.getKey(), locations);
+  }
+
+  void clear()
+  {
+    try
+    {
+      preferences.clear();
+    }
+    catch (final BackingStoreException e)
+    {
+      e.printStackTrace();
+    }
+    System.out.println("User preferences cleared.");
+  }
+
+  void listAllPreferences()
+  {
+    System.out.println("User preferences:");
+    try
+    {
+      preferences.exportNode(System.out);
+    }
+    catch (final IOException e)
+    {
+      e.printStackTrace();
+    }
+    catch (final BackingStoreException e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+}
