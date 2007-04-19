57782465540a0a3a4d6044c2ba3b3dcec734d7c2
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2007-04-19 02:38:39 +0000

    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@98 68adf913-14b5-4d52-97c4-70b13c5eca14

--- daylightchart/src/main/java/daylightchart/gui/DaylightChartGui.java
+++ daylightchart/src/main/java/daylightchart/gui/DaylightChartGui.java
@@ -22,17 +22,13 @@
 package daylightchart.gui;
 
 
-import java.awt.CheckboxMenuItem;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.MenuItem;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.Vector;
