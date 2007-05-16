4bc862dc455086d3b62dcbcc345dc60e0f347236
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2007-05-16 02:56:36 +0000

    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@184 68adf913-14b5-4d52-97c4-70b13c5eca14

--- daylightchart/src/main/resources/daylightchart.sh
+++ daylightchart/src/main/resources/daylightchart.sh
@@ -1,1 +1,1 @@
-javaw -cp daylightchart-1.3.jar:lib/antlr-2.7.7.jar:lib/jcommon-1.0.8.jar:lib/jfreechart-1.0.4.jar:lib/joda-time-1.4.jar:lib/pointlocation6709-1.3.jar;lib/looks-2.1.2.jar daylightchart.Main
+javaw -cp lib/daylightchart-1.3.jar:lib/antlr-2.7.7.jar:lib/jcommon-1.0.8.jar:lib/jfreechart-1.0.4.jar:lib/joda-time-1.4.jar:lib/pointlocation6709-1.3.jar;lib/looks-2.1.2.jar daylightchart.Main
