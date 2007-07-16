2264ad2a08b2434bd09501ba67907f8a1175edf0
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2007-07-16 17:20:04 +0000

    Changing version # to 1.6
    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@278 68adf913-14b5-4d52-97c4-70b13c5eca14

--- daylightchart/src/main/resources/daylightchart.sh
+++ daylightchart/src/main/resources/daylightchart.sh
@@ -1,1 +1,1 @@
-java -cp ./lib/antlr-2.7.7.jar:./lib/commons-lang-2.3.jar:./lib/daylightchart-1.5.jar:./lib/forms-1.0.5.jar:./lib/jcommon-1.0.8.jar:./lib/jfreechart-1.0.4.jar:./lib/joda-time-1.4.jar:./lib/looks-2.1.2.jar:./lib/pointlocation6709-1.5.jar daylightchart.Main
+java -cp ./lib/antlr-2.7.7.jar:./lib/commons-lang-2.3.jar:./lib/daylightchart-1.6.jar:./lib/forms-1.0.5.jar:./lib/jcommon-1.0.8.jar:./lib/jfreechart-1.0.4.jar:./lib/joda-time-1.4.jar:./lib/looks-2.1.2.jar:./lib/pointlocation6709-1.6.jar daylightchart.Main
