97c58a0cc04629748f06980a18d57cc4ebc97cbf
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2007-08-08 01:19:49 +0000

    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@335 68adf913-14b5-4d52-97c4-70b13c5eca14

--- daylightchart/src/main/java/daylightchart/chart/ChartConfiguration.java
+++ daylightchart/src/main/java/daylightchart/chart/ChartConfiguration.java
@@ -18,8 +18,8 @@ public final class ChartConfiguration
   public static final Dimension chartDimension = new Dimension(770, 570);
   public static final DateFormat monthsFormat = new SimpleDateFormat("MMM");
 
-  public static final Color daylightColor = new Color(0xFF, 0xFF, 0x40, 190);
-  public static final Color twilightColor = new Color(0xFF, 0x99, 0x00, 50);
+  public static final Color daylightColor = new Color(0xFF, 0xFF, 0x60, 190);
+  public static final Color twilightColor = new Color(0xFF, 0xFF, 0x40, 190);
   public static final Color nightColor = new Color(75, 11, 91, 190);
   public static final Font chartFont = new Font("Helvetica", Font.PLAIN, 12);
 
