9f839dda9fe45413a76e27ae2db4022e2c985df7
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2007-08-15 12:06:58 +0000

    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@350 68adf913-14b5-4d52-97c4-70b13c5eca14

--- daylightchart/src/test/java/daylightchart/test/TestLocation.java
+++ daylightchart/src/test/java/daylightchart/test/TestLocation.java
@@ -64,7 +64,7 @@ public class TestLocation
     final InputStreamReader reader = new InputStreamReader(dataStream);
     List<Location> locations = LocationParser.parseLocations(reader);
 
-    assertEquals(106, locations.size());
+    assertEquals(107, locations.size());
   }
 
 }
