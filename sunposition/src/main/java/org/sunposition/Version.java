5d877d434cea3ff988deda9cddb9fc8b1ab7cf1b
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2007-09-10 01:57:42 +0000

    Version 2.2
    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@398 68adf913-14b5-4d52-97c4-70b13c5eca14

--- sunposition/src/main/java/org/sunposition/Version.java
+++ sunposition/src/main/java/org/sunposition/Version.java
@@ -33,7 +33,7 @@ public final class Version
 {
 
   private static final String PRODUCTNAME = "Sun Position Calculations"; //$NON-NLS-1$
-  private static final String VERSION = "2.1"; //$NON-NLS-1$
+  private static final String VERSION = "2.2"; //$NON-NLS-1$
 
   /**
    * Information about this product.
