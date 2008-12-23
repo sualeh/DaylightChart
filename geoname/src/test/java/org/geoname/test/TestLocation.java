4a9b156a0e926b0f2d5e5cd84c408ed10f850c70
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2008-12-23 20:39:11 +0000

    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@548 68adf913-14b5-4d52-97c4-70b13c5eca14

--- geoname/src/test/java/org/geoname/test/TestLocation.java
+++ geoname/src/test/java/org/geoname/test/TestLocation.java
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
-package daylightchart.test;
+package org.geoname.test;
 
 
 import static org.junit.Assert.assertEquals;
