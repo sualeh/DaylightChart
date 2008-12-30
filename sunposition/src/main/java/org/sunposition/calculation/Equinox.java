a386453c0743c786afef429b99dfa42add88978b
sfatehi <sfatehi@68adf913-14b5-4d52-97c4-70b13c5eca14>
2008-12-30 19:16:26 +0000

    git-svn-id: http://svn.code.sf.net/p/daylightchart/code@568 68adf913-14b5-4d52-97c4-70b13c5eca14

--- sunposition/src/main/java/org/sunposition/calculation/Equinox.java
+++ sunposition/src/main/java/org/sunposition/calculation/Equinox.java
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
  */
-package daylightchart.sunchart.calculation;
+package org.sunposition.calculation;
 
 
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
