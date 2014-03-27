91ebd0fb07027037d2ad6964a7426bd8d8552e5e
Sualeh Fatehi <sualeh@hotmail.com>
2014-03-26 22:26:56 -0400

    Compiling with Java 8 Changing version from 3.4 to 4.0

--- daylightchart/src/main/java/daylightchart/Version.java
+++ daylightchart/src/main/java/daylightchart/Version.java
@@ -33,7 +33,7 @@ public final class Version
 {
 
   private static final String PRODUCTNAME = "Daylight Chart"; //$NON-NLS-1$
-  private static final String VERSION = "3.4"; //$NON-NLS-1$
+  private static final String VERSION = "4.0"; //$NON-NLS-1$
 
   /**
    * Information about this product.
